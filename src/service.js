674c9fd7cd9994ac4626e179e66d3fc25fe8eb8a
gemayc <gemita284@hotmail.com>
2025-07-06 20:54:19 +0200

    delete topics

--- src/service.js
+++ src/service.js
@@ -24,11 +24,17 @@ let moviesContainer = document.querySelector('section');
 
 async function printMovies() {
   const movies = await getMovies();
-  const movieList = movies.map(movie => {
-    return moviesContainer.innerHTML += 
+  // const movieList = movies.map(movie => {
+  //   return moviesContainer.innerHTML += //+=:  x = 2;  x = x + 1  =   x += 1
+  //   `<h1>${movie.title}</h1>
+  //   <p>${movie.scienceField}</p>`;
+  // });
+
+  movies.map(movie => {
+    moviesContainer.innerHTML += //+=:  x = 2;  x = x + 1  =   x += 1
     `<h1>${movie.title}</h1>
-    <p>${movie.scienceFieldEs}</p>`;
+    <p>${movie.scienceField}</p>`;
   });
 
-  return movieList;
+  // return movieList;
 }
